b47a27e233530d7a3268ae64fcf4eb69628b7088
Jon-Millent <millent@126.com>
2018-11-22 15:31:23 +0800

    fix bug

--- test/test.js
+++ test/test.js
@@ -4,8 +4,8 @@ let miniQrcode = require('../core/index')
 const fs = require('fs')
 
 let qrocode = new miniQrcode({
-  appId: 'wxbd0737aaa75b002a',
-  appSecret: 'e2b7d2588af69387e1377bf58d4d1ff3'
+  appId: '',
+  appSecret: ''
 });
 
 
